3a35ff929bdc467699132ea13fbc384126d11ef0
Paramveer Singh <paramveersingh131203@gmail.com>
2024-05-04 18:26:45 +0530

    Fourth Commit

--- core/urls.py
+++ core/urls.py
@@ -4,14 +4,11 @@ from diseasePrediction import views as v
 
 urlpatterns = [
 	path('', views.home, name='home'),
-	path('Disease-Predictor',v.diagnosis,name='diagnosis'),
-	path('BMI-Calculator', views.bmi, name='bmi'),
-	#path('Daily-Calories-Requirement', views.calorie, name='calorie'),
-	path('Workout-Plans', views.home, name='plans'),
-	path('Blog', views.blog, name='blog'),
-	path('Reviews', views.reviews, name='review'),
-	path('Home', views.home, name='home'),
-	path('Daily-Calories-Requirement/', views.calorie, name='home'),
-	path('', views.home, name='home'),
-	path('', views.home, name='home'),
+	path('Disease-Predictor/',v.diagnosis,name='diagnosis'),
+	path('BMI-Calculator/', views.bmi, name='bmi'),
+	path('Daily-Calories-Requirement/', views.calorie, name='calorie'),
+	path('Workout-Plans/', views.home, name='plans'),
+	path('Blog/', views.blog, name='blog'),
+	path('Reviews/', views.reviews, name='reviews'),
+	path('Nutrition-Guidance/', views.home, name='nutrition'),
 ]
